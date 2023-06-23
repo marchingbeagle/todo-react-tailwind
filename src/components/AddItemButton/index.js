0b2766258f7556f1661c116816e9efa05dcef9f5
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-23 01:18:26 -0300

    fontawesome added and add item button changed

--- src/components/AddItemButton/index.js
+++ src/components/AddItemButton/index.js
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 function AddItemButton({ addToList }) {
   return (
-    <button onClick={addToList} className="h-10 w-10 bg-blue-400 rounded-full">
-      +
+    <button
+      onClick={addToList}
+      className="h-10 w-10 bg-blue-300 hover:bg-blue-400 transition rounded-lg"
+    >
+      <FontAwesomeIcon icon={faPlus} color="white" size="lg" />
     </button>
   );
 }
